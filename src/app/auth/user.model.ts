82d33715226ff4e2d714be5786cc619e82a2d598
Juan Fernando <juanferrgiraldo@gmail.com>
2018-02-27 09:53:27 -0500

    Login funcionando, faalta aplicar estilos.

--- src/app/auth/user.model.ts
+++ src/app/auth/user.model.ts
@@ -2,6 +2,7 @@ export class User {
     constructor(
         public email: string,
         public password: string,
+        // public id?: string,
         public name?: string,
         public user?: string
     ) {}
