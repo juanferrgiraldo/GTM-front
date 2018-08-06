598ccc416285eada1efd06f5b00d7f2b536cefcb
Juan Fernando <juanferrgiraldo@gmail.com>
2018-08-06 08:21:47 -0500

    Adding signup validators.

--- src/app/fireauth/firesignin/firesignin.component.ts
+++ src/app/fireauth/firesignin/firesignin.component.ts
@@ -5,20 +5,19 @@ import { Router } from '@angular/router';
 @Component({
     selector: 'app-firesignin',
     templateUrl: './firesignin.component.html',
-    styleUrls: ['./firesignin.component.css'] 
+    styleUrls: ['./firesignin.component.css']
 })
 
 export class FiresigninComponent implements OnInit {
     signinGroup: FormGroup;
 
-    ngOnInit(){
-        this.signinGroup = new FormGroup({            
+    ngOnInit() {
+        this.signinGroup = new FormGroup({
             'email': new FormControl(null, [
                 Validators.required,
                 Validators.email
             ]),
-            'password': new FormControl(null, Validators.required)         
-        })
+            'password': new FormControl(null, Validators.required)
+        });
     }
-    
 }
--- src/app/fireauth/firesignup/firesignup.component.ts
+++ src/app/fireauth/firesignup/firesignup.component.ts
@@ -1,27 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { FormGroup, FormControl, AbstractControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-firesignup',
     templateUrl: './firesignup.component.html',
-    styleUrls: ['./firesignup.component.css'] 
+    styleUrls: ['./firesignup.component.css']
 })
-
 export class FiresignupComponent implements OnInit {
-    signupGroup: FormGroup;
 
-    ngOnInit(){
-        this.signupGroup = new FormGroup({            
-            'email': new FormControl(null, [
-                Validators.required,
-                Validators.email
-            ]),
-            'passwords': new FormGroup({
-                'password': new FormControl(null, Validators.required),
-                'repassword': new FormControl(null, Validators.required),
-            })           
-        })
+  signupForm: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.signupForm = this.fb.group ({
+      email: [null, [Validators.required, Validators.email]],
+      passwords: this.fb.group ({
+        password: [null, Validators.required],
+        repeatPassword: [null, [Validators.required]]
+      }, { validator: this.matchPasswords})
+    });
+  }
+
+  matchPasswords(ac: AbstractControl) {
+    const password = ac.get('password').value;
+    const repeatPassword = ac.get('repeatPassword').value;
+    if (password !== repeatPassword) {
+      ac.get('repeatPassword').setErrors({matchPasswords: true});
     }
-    
+    ac.get('repeatPassword').setErrors({matchPasswords: false});
+  }
+
 }
